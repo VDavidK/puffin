7027b24e36632639a2f6d634e1fbc7d1a7fac231
Axel Snaer <axelatlason2003@gmail.com>
2026-05-06 16:13:53 +0000

    Add utility functions for dealing with native components

--- puffin_stdlib/src/base/elements/mod.rs
+++ puffin_stdlib/src/base/elements/mod.rs
@@ -1,4 +1,43 @@
+use puffin_runtime::runtime::Runtime;
+use puffin_runtime::RuntimeError;
+use puffin_runtime::value::{ComponentNode, DictionaryType, ListType, NodeType, Value};
+
 pub mod text;
 pub mod flow;
 pub mod frame;
 pub mod input;
+
+pub(crate) type Props = DictionaryType;
+
+pub(crate) fn get_props(runtime: &mut Runtime) -> Result<Props, RuntimeError> {
+    Ok(runtime.get_local(-1)?
+        .to_owned()
+        .take_dictionary()?)
+}
+
+pub(crate) fn get_inner(runtime: &mut Runtime) -> Result<Value, RuntimeError> {
+    Ok(runtime.get_local(-1)?.to_owned())
+}
+
+pub(crate) fn get_inner_nodes(runtime: &mut Runtime) -> Result<Vec<NodeType>, RuntimeError> {
+    Ok(get_inner_as::<ListType>(runtime)?
+        .borrow()
+        .iter()
+        .cloned()
+        .map(|v| ComponentNode::try_from(v.take_instance()?).map(Into::into))
+        .collect::<Result<Vec<_>, _>>()?)
+}
+
+pub(crate) fn get_inner_as<T: TryFrom<Value, Error = RuntimeError>>(runtime: &mut Runtime) -> Result<T, RuntimeError> {
+    T::try_from(runtime.get_local(-1)?.to_owned())
+}
+
+pub(crate) fn fetch_property<T: TryFrom<Value, Error = RuntimeError>>(props: &Props, name: impl Into<String>) -> Result<T, RuntimeError> {
+    let name = name.into();
+    let value = props
+        .borrow()
+        .get(&name.to_owned().into())
+        .ok_or_else(|| RuntimeError::NoFieldMatchingName { name })?
+        .to_owned();
+    T::try_from(value)
+}
